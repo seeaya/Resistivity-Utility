5b3710cfb0fd79d5cdfd7792b1b591620970c0ad
seeaya <connorbarnesdenver@icloud.com>
2019-07-01 15:15:00 -0600

    Initial Commit

---
+++ Resistivity Measurer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Resistivity Measurer
+//
+//  Created by Connor Barnes on 7/1/19.
+//  Copyright © 2019 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Resistivity Measurer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Resistivity Measurer
+//
+//  Created by Connor Barnes on 7/1/19.
+//  Copyright © 2019 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
